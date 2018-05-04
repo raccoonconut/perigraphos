caabcc7e39fbfeb6758e2e37fa9b5d50e5081d28
raccoonconut <raccoonconut@protonmail.com>
2018-05-04 13:05:57 +0300

    add indradb
    Signed-off-by: raccoonconut <raccoonconut@protonmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import os
+from subprocess import Popen
 from PyQt5.QtWidgets import (QApplication,
 QWidget, QFileDialog, QOpenGLWidget, QMainWindow,
 QPushButton, QVBoxLayout, QHBoxLayout, QLayout, QGridLayout)
@@ -8,6 +9,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5 import QtSvg
 
 from canvas import MyCanvas
+from indradb import *
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -26,7 +28,6 @@ class App(QMainWindow):
         self.mainLayout.sizeConstraint = QLayout.SetDefaultConstraint
         self.mainLayout.addWidget(self.canvas)
         self.mainLayout.addLayout(self.ctrlWidgets.get_layout())
-
         self.initUI()
 
     def initUI(self):
@@ -47,17 +48,26 @@ class App(QMainWindow):
         # self.openFileNameDialog()
 
 
+path = '/home/orestes/Workspace/bin/indradb/'
+server_proc = None
+
+
 class ControlWidgets(QWidget):
 
     def __init__(self, parent):
         super(ControlWidgets, self).__init__(parent)
         self.__controls()
         self.__layout()
+        self.button1.clicked.connect(self.call_indradb)
+        self.button2.clicked.connect(Database.stop_server)
+        self.button3.clicked.connect(Database.stop_server)
+
+        self.server_proc = None
 
     def __controls(self):
-        self.button1 = QPushButton("action1")
-        self.button2 = QPushButton("action2")
-        self.button3 = QPushButton("action3")
+        self.button1 = QPushButton("start")
+        self.button2 = QPushButton("stop")
+        self.button3 = QPushButton("connect")
 
     def __layout(self):
         self.vbox = QVBoxLayout()
@@ -70,9 +80,39 @@ class ControlWidgets(QWidget):
     def get_layout(self):
         return self.vbox
 
+    def call_indradb(self):
+        # server_proc = subprocess.Popen([dbPath + "indradb-server"], stdout=sys.stdout, stderr=sys.stderr)
+        Database.start_server(path)
+        # client = Client('0.0.0.0', request_timeout=60, scheme='https')
+        print('hello')
+
+    def connect_client(self):
+        try:
+            Database.connect_client()
+        except Error as e:
+            print(e)
+
+
+class Database(object):
+    proc = None
+    client = None
+
+    @staticmethod
+    def start_server(dbPath):
+        Database.proc = Popen([dbPath + "indradb-server"], stdout=sys.stdout, stderr=sys.stderr)
+
+    @staticmethod
+    def stop_server(self):
+        Database.proc.kill()
+
+    @staticmethod
+    def connect_client(self):
+        Database.client = Client('0.0.0.0', request_timeout=60, scheme='https')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
     ex.show()
     sys.exit(app.exec_())
+
